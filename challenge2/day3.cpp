8597f1db92a13b1758e2296d3d69fcb51aeadb15
pofox <66141963+pofox@users.noreply.github.com>
2024-10-27 10:14:39 +0300

    day3

---
+++ challenge2/day3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+void Swap(std::vector<int>* numbers, int idx1, int idx2)
+{
+	(*numbers)[idx1] ^= (*numbers)[idx2];
+	(*numbers)[idx2] ^= (*numbers)[idx1];
+	(*numbers)[idx1] ^= (*numbers)[idx2];
+}
+
+void BubbleSort(std::vector<int>* numbers)
+{
+	for (int i = 0; i < numbers->size() - 1; i++)
+	{
+		for (int j = 1; j < (*numbers).size() - i; j++)
+		{
+			if ((*numbers)[i + j] > (*numbers)[j]) Swap(numbers, i, j);
+		}
+	}
+}
+
+void PrintVector(std::vector<int> numbers)
+{
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << "\t";
+	}std::cout << "\n";
+}
