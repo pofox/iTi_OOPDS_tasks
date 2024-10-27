f6e7decfe9100271be56195e40bf2f62257b1d2c
pofox <66141963+pofox@users.noreply.github.com>
2024-10-27 15:21:39 +0300

    Update day3.cpp

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -1,40 +1,6 @@
 #include <iostream>
 #include <vector>
-
-void Swap(std::vector<int>* numbers, int idx1, int idx2)
-{
-	(*numbers)[idx1] ^= (*numbers)[idx2];
-	(*numbers)[idx2] ^= (*numbers)[idx1];
-	(*numbers)[idx1] ^= (*numbers)[idx2];
-}
-
-void BubbleSort(std::vector<int>* numbers)
-{
-	for (int i = 0; i < (*numbers).size() - 1; i++)
-	{
-		for (int j = 0; j < (*numbers).size() - i - 1; j++)
-		{
-			if ((*numbers)[j] > (*numbers)[j+1]) Swap(numbers, j, j + 1);
-		}
-	}
-}
-
-std::vector<int> InsetionSort(std::vector<int> numbers)
-{
-	int j;
-	for (int i = 1; i < numbers.size(); i++)
-	{
-		int key = numbers[i];
-		j = i - 1;
-		while(j >= 0 && key < numbers[j])
-		{
-			numbers[j + 1] = numbers[j];
-			j--;
-		}
-		numbers[j + 1] = key;
-	}
-	return numbers;
-}
+#include <string>
 
 std::vector<int> MergeSort(std::vector<int> numbers)
 {
@@ -95,20 +61,30 @@ std::vector<int> MergeSort(std::vector<int> numbers)
 	return numbers;
 }
 
-void PrintVector(std::vector<int> numbers)
+struct Node
 {
-	for (int i = 0; i < numbers.size(); i++)
+	std::string name;
+	int ID;
+	Node* next;
+	Node* prev;
+	void printemployee()
 	{
-		std::cout << numbers[i] << "\t";
+		std::cout<<"Name : "<< name<<"ID : "<< ID<<"\n";
 	}
-	std::cout << "\n";
+}
+
+struct DLinkedList
+{
+	Node* first=NULL;
+	Node* last=NULL;
+	void add(Node* node)
+	{
+		
+	}
 }
 
 int main()
 {
-	std::vector<int> arr = { 4,3,6,2,6,7,3,9,-2 };
-	PrintVector(arr);
-	arr = MergeSort(arr);
-	PrintVector(arr);
+	
 }
 
