884139fa96716bf0e014107148ce3f835ed38b0a
pofox <66141963+pofox@users.noreply.github.com>
2024-10-27 16:52:18 +0300

    .

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -2,71 +2,12 @@
 #include <vector>
 #include <string>
 
-std::vector<int> MergeSort(std::vector<int> numbers)
-{
-	int size = numbers.size();
-	if (size == 2)
-	{
-		if (numbers[0] > numbers[1])
-		{
-			Swap(&numbers, 0, 1);
-			return numbers;
-		}
-		return numbers;
-	}
-	if (size == 1) return numbers;
-
-	std::vector<int> vec1;
-	std::vector<int> vec2;
-
-	for (int i = 0; i < size / 2; i++)
-	{
-		vec1.push_back(numbers[i]);
-	}
-	for (int i = size / 2; i < size; i++)
-	{
-		vec2.push_back(numbers[i]);
-	}
-
-	vec1 = MergeSort(vec1);
-	vec2 = MergeSort(vec2);
-
-	int i, j, k;
-	i = j = k = 0;
-	for(; i < size && j < size / 2 && k < size - (int)(size / 2); i++)
-	{
-		if (vec1[j] < vec2[k])
-		{
-			numbers[i] = vec1[j];
-			j++;
-		}
-		else
-		{
-			numbers[i] = vec2[k];
-			k++;
-		}
-	}
-	while (j < size / 2)
-	{
-		numbers[i] = vec1[j];
-		i++;
-		j++;
-	}
-	while (k < size - (int)(size / 2))
-	{
-		numbers[i] = vec2[k];
-		i++;
-		k++;
-	}
-	return numbers;
-}
-
 struct Node
 {
 	std::string name;
 	int ID;
-	Node* next;
-	Node* prev;
+	Node* next = nullptr;
+	Node* prev = nullptr;
 	void printemployee()
 	{
 		std::cout<<"Name : "<< name<<"ID : "<< ID<<"\n";
@@ -77,14 +18,151 @@ struct DLinkedList
 {
 	Node* first=NULL;
 	Node* last=NULL;
+	int size = 0;
 	void add(Node* node)
 	{
-		
+		size++;
+		if (first == NULL)
+		{
+			first = node;
+			last = node;
+			return;
+		}
+		node->prev = last;
+		last->next = node;
+		last = node;
+	}
+	void removeat(int idx)
+	{
+		size--;
+		Node* node=first;
+		while (idx >= 0)
+		{
+			node = node->next;
+			idx--;
+		}
+		if (node->prev != NULL) *(node->prev)->next = node->next;
+		if (node->prev == NULL) first = node->next;
+		if (node->next != NULL) *(node->next)->prev = node->prev;
+		if (node->next == NULL) last = node->prev;
+	}
+	int find(int id)
+	{
+		Node* node = first;
+		int idx = 0;
+		while (id != node->ID)
+		{
+			if (node->next == NULL) return -1;
+			node = node->next;
+			idx++;
+		}
+		return idx;
+	}
+	void replaceAt(Node* newnode, int idx)
+	{
+		Node* node = first;
+		while (idx >= 0)
+		{
+			node = node->next;
+			idx--;
+		}
+		if (node->prev != NULL) *(node->prev)->next = newnode;
+		if (node->prev == NULL) 
+		{ 
+			first = newnode;
+			newnode->next = node->next;
+		}
+		if (node->next != NULL) *(node->next)->prev = newnode;
+		if (node->next == NULL) 
+		{
+			last = newnode;
+			newnode->prev = node->prev;
+		}
+	}
+	void printall()
+	{
+		Node* node = first;
+		while (node->next != NULL)
+		{
+			node->printemployee();
+		}
+	}
+	DLinkedList MergeSort(DLinkedList d)
+	{
+		if (d.size == 2)
+		{
+			if (d.first->ID > (*(d.first)->next).ID)
+			{
+				Node* node=d.first;
+				replaceAt(*(d.first)->next, 0);
+				replaceAt(node, 1);
+				return d;
+			}
+			return d;
+		}
+		if (size == 1) return d;
+
+		DLinkedList l1;
+		DLinkedList l2;
+
+		Node* node = d.first;
+		for (int i = 0; i < d.size / 2; i++)
+		{
+			l1.add(node);
+			node = node->next;
+		}
+		for (int i = d.size / 2; i < d.size; i++)
+		{
+			l2.add(d);
+			node = node->next;
+		}
+
+		l1 = MergeSort(l1);
+		l2 = MergeSort(l2);
+
+		int i;
+		Node* j = l1.first;
+		Node* k = l2.first;
+		i = j = k = 0;
+		for (; i < d.size && j->next != NULL && k->next != NULL; i++)
+		{
+			if (j->ID < k->ID)
+			{
+				d.replaceAt(j, i);
+				j = j->next;
+			}
+			else
+			{
+				d.replaceAt(k, i);
+				k = k->next;
+			}
+		}
+		while (j != NULL)
+		{
+			d.replaceAt(j,i)
+			i++;
+			j = j->next;
+		}
+		while (k != NULL)
+		{
+			d.replaceAt(k, i);
+			i++;
+			k = k->next;
+		}
 	}
 }
 
 int main()
 {
-	
+	DLinkedList list;
+
+	Node a = { "mohamed",1 };
+	Node b = { "khaled",3 };
+	Node c = { "Amr",2 };
+
+	list.add(&a);
+	list.add(&b);
+	list.add(&c);
+	list.printall();
 }
 
