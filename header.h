639cd3fa21e2b25a9cd5884a868a4afaf07ead1e
pofox <66141963+pofox@users.noreply.github.com>
2024-10-31 13:09:01 +0300

    Update header.h

--- header.h
+++ header.h
@@ -2,31 +2,13 @@
 class ComplexNumber
 {
 public:
-    void ComplexNumber(float x,float y);
     float x;
     float y;
+    ComplexNumber(float x,float y);
 };
 
-ComplexNumber Add(ComplexNumber z1,ComplexNumber z2)
-{
-    ComplexNumber out;
-    out.x = z1.x + z2.x;
-    out.y = z1.y + z2.y;
-    return out;
-}
+ComplexNumber Add(ComplexNumber z1,ComplexNumber z2);
 
-ComplexNumber Sub(ComplexNumber z1,ComplexNumber z2)
-{
-    ComplexNumber out;
-    out.x = z1.x - z2.x;
-    out.y = z1.y - z2.y;
-    return out;
-}
+ComplexNumber Sub(ComplexNumber z1,ComplexNumber z2);
 
-ComplexNumber Mul(ComplexNumber z1,ComplexNumber z2)
-{
-    ComplexNumber out;
-    out.x = z1.x * z2.x - z1.y * z2.y;
-    out.y = z1.x * z2.y + x1.y * z2.x;
-    return out;
-}
+ComplexNumber Mul(ComplexNumber z1,ComplexNumber z2);
