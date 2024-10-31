7da1a37630dd83aff9f73adc15f49794458a3521
pofox <66141963+pofox@users.noreply.github.com>
2024-10-31 16:04:08 +0300

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -4,4 +4,45 @@ ComplexNumber::ComplexNumber(float a,float b)
 {
     x = a;
     y = b;
+    count++;
+}
+ComplexNumber::ComplexNumber(float a)
+{
+    x = a;
+    y = 0;
+    count++;
+}
+ComplexNumber::ComplexNumber()
+{
+    x = 0;
+    y = 0;
+    count++;
+}
+ComplexNumber::ComplexNumber(const ComplexNumber& z)
+{
+    x=z.GetRe();
+    y=z.GetIm();
+    count++;
+}
+void ComplexNumber::SetRe(float a)
+{
+    x = a;
+}
+void ComplexNumber::SetIm(float b)
+{
+    y = b;
+}
+float ComplexNumber::GetRe() const
+{
+    return x;
+}
+
+float ComplexNumber::GetIm() const
+{
+    return y;
+}
+
+ComplexNumber::~ComplexNumber()
+{
+    count--;
 }
