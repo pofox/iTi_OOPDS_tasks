57857844e28275215563d3cc2b4d8b8ade585fb4
pofox <66141963+pofox@users.noreply.github.com>
2024-10-31 13:08:38 +0300

    Update day5.cpp

--- day5.cpp
+++ day5.cpp
@@ -1,22 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <Windows.h>
+//#include <Windows.h>
 #include <string>
 #include "header.h"
 
+ComplexNumber Add(ComplexNumber z1,ComplexNumber z2)
+{
+    ComplexNumber out(0,0);
+    out.x = z1.x + z2.x;
+    out.y = z1.y + z2.y;
+    return out;
+}
+
+ComplexNumber Sub(ComplexNumber z1,ComplexNumber z2)
+{
+    ComplexNumber out(0,0);
+    out.x = z1.x - z2.x;
+    out.y = z1.y - z2.y;
+    return out;
+}
+
+ComplexNumber Mul(ComplexNumber z1,ComplexNumber z2)
+{
+    ComplexNumber out(0,0);
+    out.x = z1.x * z2.x - z1.y * z2.y;
+    out.y = z1.x * z2.y + z1.y * z2.x;
+    return out;
+}
+
 void CompNum()
 {
-    ComplexNumber z1,z2,result;
-    z1.ComplexNumber(1,2);
-    z2.ComplexNumber(3,4);
+    ComplexNumber z1(1,2);
+	ComplexNumber z2(3,4);
+	ComplexNumber result(0,0);
     std::cout<<"z1 = "<<z1.x<<"+i"<<z1.y<<"\n";
     std::cout<<"z2 = "<<z2.x<<"+i"<<z2.y<<"\n";
     result = Add(z1,z2);
-    std::cout<<"z1+z2 = "<<result.x<<"+i"<<result.y<<"\n";
+    std::cout<<"z1+z2 = "<<result.x;
+	if (result.y>0) std::cout<<"+";
+    std::cout<<result.y<<"i\n";
     result = Sub(z1,z2);
-    std::cout<<"z1-z2 = "<<result.x<<"+i"<<result.y<<"\n";
-    result = Mult(z1,z2);
-    std::cout<<"z1*z2 = "<<result.x<<"+i"<<result.y<<"\n";
+    std::cout<<"z1-z2 = "<<result.x;
+	if (result.y>0) std::cout<<"+";
+    std::cout<<result.y<<"i\n";
+    result = Mul(z1,z2);
+    std::cout<<"z1*z2 = "<<result.x;
+	if (result.y>0) std::cout<<"+";
+    std::cout<<result.y<<"i\n";
 }
 
 
@@ -42,7 +72,7 @@ int main()
 		case 'd':
 			return 0;
 		case 'e':
-			Windows("cls");
+			//Windows("cls");
 			break;
 		case 'f':
 			//ascii();
